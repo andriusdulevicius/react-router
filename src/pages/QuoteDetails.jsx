c80dbca0e1319e4156077ac5129d45068713f1a5
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-27 11:00:09 +0300

    main nav added

--- src/pages/QuoteDetails.jsx
+++ src/pages/QuoteDetails.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useParams, Route } from 'react-router-dom';
 import QuoteItem from '../components/quotes/QuoteItem';
+import Comments from '../components/comments/Comments';
 
 const QuoteDetails = () => {
+  const { quoteId } = useParams();
+
   return (
     <div>
-      <h2>One quote item</h2>
-      <QuoteItem />
+      <h2>One quote item with id {quoteId}</h2>
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
