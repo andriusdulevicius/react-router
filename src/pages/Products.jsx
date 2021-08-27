15b1a42360a9af65f64520565875fb4776c720f8
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-27 09:21:13 +0300

    redirect

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,13 +6,13 @@ const Products = () => {
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to='/product-details/boat'>A boat</Link>
+          <Link to='/products/boat'>A boat</Link>
         </li>
         <li>
-          <Link to='/product-details/bread'>A bread</Link>
+          <Link to='/products/bread'>A bread</Link>
         </li>
         <li>
-          <Link to='/product-details/car'>A car</Link>
+          <Link to='/products/car'>A car</Link>
         </li>
       </ul>
     </>
